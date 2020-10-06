30f6173a19f66856490147779d1aedb1a15281c7
letuongquy <72064367+letuongquy@users.noreply.github.com>
2020-10-06 14:15:18 +0700

    Add files via upload

---
+++ Bai1.java
@@ -0,0 +1,19 @@
+package lap3;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Bai1 {
+    public static void main(String[] args) {
+		try {
+			InetAddress host = InetAddress.getByName("www.youtube.com");
+			String hostName = host.getHostName();
+			System.out.println("Host name:"+hostName);
+			System.out.println("Dia chi IP"+host.getHostAddress());
+		}
+		catch(UnknownHostException e) {
+			System.out.println("Khong tim thay dia chi");
+			return;
+		}
+	}
+}
---
+++ Bai6.java
@@ -0,0 +1,35 @@
+package lap3;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Bai6 {
+    public static void main(String[] args) {
+		if(args.length>0) {
+			try {
+				URL u =new URL(args[0]);
+				URLConnection uc = u.openConnection();
+				InputStream raw= uc.getInputStream();
+				InputStream buffer = new BufferedInputStream(raw);
+				
+				Reader r = new InputStreamReader(buffer);
+				int c;
+				while((c=r.read())!=-1) {
+					System.out.print((char)c);
+				}
+			}
+			catch (MalformedURLException ex) {
+				System.err.println(args[0]+"is not parseable");
+			}
+			catch(IOException ex) {
+				System.err.println(ex);
+			}
+		}
+	}
+}
